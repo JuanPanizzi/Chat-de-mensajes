aa8aeb97bf36c5836316ddcb1d73da3ec233bca1
Juan <juanpanizzi@hotmail.com>
2024-04-14 21:17:30 -0300

    cors policy updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,22 +2,42 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Server } from 'socket.io';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'https://chat-de-mensajes-client.onrender.com',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
 
+  const server = app.getHttpServer();
 
-  app.use(cors({
-    origin: 'https://chat-de-mensajes-client.onrender.com' || '*', // o '*' para permitir cualquier origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  }));
+  // Configurar socket.io en el mismo servidor HTTP de NestJS
+  const io = new Server(server, {
+    cors: {
+      origin: 'https://chat-de-mensajes-client.onrender.com',
+      methods: ['GET', 'POST'],
+      credentials: true,
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('Cliente conectado a través de WebSocket:', socket.id);
+    // Aquí puedes implementar la lógica para manejar eventos de socket.io
+  });
+
+
+  await app.listen(port);
+
+
+
   //2
   // app.useWebSocketAdapter(new CustomSocketIoAdapter(app));
 
@@ -38,25 +58,8 @@ async function bootstrap() {
   //   },
   // }));
 
-  //WEBSOCKETS CORS
-  // Crear servidor HTTP
-  const server = app.getHttpServer();
 
-  // Configurar socket.io con opciones CORS
-  const io = new Server(server, {
-    cors: {
-      origin: 'https://chat-de-mensajes-client.onrender.com',
-      methods: ['GET', 'POST'],
-      credentials: true,
-    },
-  });
-  // Lógica de manejo de conexiones de socket.io
-  io.on('connection', (socket) => {
-    console.log('Cliente conectado a través de WebSocket:', socket.id);
-    // Aquí puedes implementar lógica para manejar eventos de socket.io
-  });
 
-  await app.listen(port);
 }
 bootstrap();
 
