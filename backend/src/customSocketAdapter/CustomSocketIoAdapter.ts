a5d83b84c14ac58d06b50d039c3ebcf37b5e3bd8
Juan <juanpanizzi@hotmail.com>
2024-04-17 20:13:05 -0300

    websockets cors policy updated again

--- backend/src/customSocketAdapter/CustomSocketIoAdapter.ts
+++ backend/src/customSocketAdapter/CustomSocketIoAdapter.ts
@@ -5,8 +5,22 @@ export class CustomSocketIoAdapter extends IoAdapter {
     createIOServer(port: number, options?: ServerOptions): any {
       const server = super.createIOServer(port, options);
   
-      // Configurar CORS según tus necesidades
-      server.origins(['https://chat-de-mensajes-client.onrender.com']);
+      const corsOptions = {
+        cors: {
+          origin: '*',
+          methods: ['GET', 'POST'],
+          allowedHeaders: ['my-custom-header'],
+          credentials: true,
+        }
+      };
+  
+      server.use((socket, next) => {
+        const request = socket.request;
+        if (request.headers.origin) {
+          corsOptions.cors.origin = request.headers.origin;
+        }
+        next();
+      });
   
       return server;
     }
