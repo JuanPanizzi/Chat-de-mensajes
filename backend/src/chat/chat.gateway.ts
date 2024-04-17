17bc91f500270c06f91bddfe1627fecd50bdfd52
Juan <juanpanizzi@hotmail.com>
2024-04-16 21:14:09 -0300

    chatgateway modified

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -2,6 +2,7 @@ import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { OnModuleInit } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
+import { createServer, Server as HTTPServer } from 'http';
 
 @WebSocketGateway()
 export class ChatGateway implements OnModuleInit  {
@@ -13,7 +14,34 @@ export class ChatGateway implements OnModuleInit  {
 
     constructor(private readonly chatService: ChatService) { }
 
+
+
+
   onModuleInit() {
+
+
+    const httpServer: HTTPServer = createServer();
+
+    // Configurar CORS en el servidor HTTP para socket.io
+    const io = require('socket.io')(httpServer, {
+      cors: {
+        origin: 'https://chat-de-mensajes-client.onrender.com' || 'http://localhost:5173/',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+      }
+    });
+
+    // Asignar el servidor socket.io al WebSocketGateway
+    this.server = io;
+
+
+
+    const port = process.env.PORT || 3000;
+    httpServer.listen(port, () => {
+      console.log(`Servidor HTTP y WebSocket iniciado en el puerto ${port}`);
+    });
+
 
     this.server.on('connection', (socket: Socket) => {
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,66 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ConfigService } from '@nestjs/config';
-import { Server } from 'socket.io';
-import * as cors from 'cors';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: 'https://chat-de-mensajes-client.onrender.com' || '*',
-  //   methods: ['GET', 'POST'],
-  //   credentials: true,
-  // });
-  app.use(cors({
-    origin: 'https://no-fumar-app.onrender.com' || '*', // o '*' para permitir cualquier origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  }));
-  
-  // const configService = app.get(ConfigService);
-  // const port = configService.get<number>('PORT', 3000);
+  const corsOptions: CorsOptions = {
+    origin: 'https://chat-de-mensajes-client.onrender.com/',
+    methods: ['GET', 'POST'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+    credentials: true, // Permite enviar cookies y headers de autenticación
+  };
 
-  // Configurar socket.io en el mismo servidor HTTP de NestJS
-  // const server = app.getHttpServer();
-
-  // const io = new Server(server, {
-  //   cors: {
-  //     origin: 'https://chat-de-mensajes-client.onrender.com',
-  //     methods: ['GET', 'POST'],
-  //     credentials: true,
-  //   },
-  // });
-  // io.on('connection', (socket) => {
-  //   console.log('Cliente conectado a través de WebSocket:', socket.id);
-  //   // Aquí puedes implementar la lógica para manejar eventos de socket.io
-  // });
-
+  app.enableCors(corsOptions);
 
   await app.listen(3000);
-
-
-
-  //2
-  // app.useWebSocketAdapter(new CustomSocketIoAdapter(app));
-
-  //3
-  // app.enableCors({
-  //   origin: '*', // Allow requests from all origins,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type,Authorization',
-  // });
-
-  //3B
-  // const httpAdapter = app.getHttpAdapter();
-  // app.useWebSocketAdapter(new IoAdapter(httpAdapter, {
-  //   cors: {
-  //     origin: '*', // Allow requests from all origins
-  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //     allowedHeaders: 'Content-Type,Authorization',
-  //   },
-  // }));
 
 
 
